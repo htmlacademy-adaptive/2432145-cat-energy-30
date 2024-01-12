599b88850d663c5a0dc819b567aeee044bb0a545
Пакри <pakri@yandex.ru>
2024-01-12 15:18:59 +0300

    Сделал скрипты на открытие мобильного меню.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,2 +1,13 @@
 /* в этот файл добавляет скрипты*/
-document.querySelector('.nav__wrapper').classList.remove('nav__wrapper--phone-menu-nojs');
+const navToggle = document.querySelector('.nav__toggle');
+const navWrapper = document.querySelector('.nav__wrapper');
+
+navToggle.classList.remove('nav__toggle--close-burger');
+navToggle.classList.add('nav__toggle--open-burger');
+navToggle.onclick = function () {
+  navToggle.classList.remove('nav__toggle--open-burger');
+  navToggle.classList.add('nav__toggle--close-burger');
+  navWrapper.classList.add('nav__wrapper--phone-menu-open');
+};
+
+navWrapper.classList.remove('nav__wrapper--phone-menu-nojs');
