351742269c3907da5c2dcc9bdfeb3617e5d4c2f1
Пакри <pakri@yandex.ru>
2024-01-12 15:28:02 +0300

    Скрипт на закрытие/открытие бургер меню.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,12 +2,12 @@
 const navToggle = document.querySelector('.nav__toggle');
 const navWrapper = document.querySelector('.nav__wrapper');
 
-navToggle.classList.remove('nav__toggle--close-burger');
-navToggle.classList.add('nav__toggle--open-burger');
-navToggle.onclick = function () {
-  navToggle.classList.remove('nav__toggle--open-burger');
-  navToggle.classList.add('nav__toggle--close-burger');
-  navWrapper.classList.add('nav__wrapper--phone-menu-open');
-};
-
 navWrapper.classList.remove('nav__wrapper--phone-menu-nojs');
+
+navToggle.classList.toggle('nav__toggle--close-burger');
+navToggle.classList.toggle('nav__toggle--open-burger');
+navToggle.onclick = function () {
+  navToggle.classList.toggle('nav__toggle--open-burger');
+  navToggle.classList.toggle('nav__toggle--close-burger');
+  navWrapper.classList.toggle('nav__wrapper--phone-menu-open');
+};
