f914e899c15a0e97cc86d2ddedc1ff6718d7274f
Пакри <pakri@yandex.ru>
2024-01-12 16:20:16 +0300

    No js скрипт для карты.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,8 +2,10 @@
 const navToggle = document.querySelector('.nav__toggle');
 const navWrapper = document.querySelector('.nav__wrapper');
 
+//Если js не работает.
 navWrapper.classList.remove('nav__wrapper--phone-menu-nojs');
 
+//Открытие/зактрытие бургер-меню.
 navToggle.classList.toggle('nav__toggle--close-burger');
 navToggle.classList.toggle('nav__toggle--open-burger');
 navToggle.onclick = function () {
@@ -11,3 +13,7 @@ navToggle.onclick = function () {
   navToggle.classList.toggle('nav__toggle--close-burger');
   navWrapper.classList.toggle('nav__wrapper--phone-menu-open');
 };
+
+//Скрипты карты.
+const addressImageWrapper = document.querySelector('.address__image-wrapper');
+addressImageWrapper.classList.remove('address__image-wrapper--no-js');
