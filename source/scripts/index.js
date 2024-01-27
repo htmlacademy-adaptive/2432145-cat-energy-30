18662b0e92c7fc77bb927c7a0c406d2adb546482
Пакри <pakri@yandex.ru>
2024-01-27 17:15:26 +0300

    SASS-02. Модульность. Убрал класс page__body--nojs. Модификатор --nojs для блоков header__wrapper, nav и address.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,14 @@
 /* в этот файл добавляет скрипты*/
-const pageBody = document.querySelector('.page__body');
+const headerWrapper = document.querySelector('.header__wrapper');
+const nav = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
 const navWrapper = document.querySelector('.nav__wrapper');
+const address = document.querySelector('.address');
 
 //Если js не работает.
-pageBody.classList.remove('page__body--nojs');
+nav.classList.remove('nav--nojs');
+headerWrapper.classList.remove('header__wrapper--nojs');
+address.classList.remove('address--nojs');
 
 //Открытие/закрытие бургер-меню.
 navToggle.onclick = function () {
